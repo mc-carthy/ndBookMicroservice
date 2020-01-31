330daeb451c0d387aa11f50f49f2a074d79976a2
Mick McCarthy <mick@mccarthy.tech>
2020-01-31 13:01:45 +0000

    Add mongoose connection

--- books/books.js
+++ books/books.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb+srv://admin:admin@bookservice-k3mbd.mongodb.net/test?retryWrites=true&w=majority', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},
+console.log('Book service db connected successfully'));
 
 app.listen(4545, () => {
     console.log('Book service listening on port 4545');
