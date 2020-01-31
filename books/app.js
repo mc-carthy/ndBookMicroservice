617ed4b131661dd19f07684e71c1aa52f179584a
Mick McCarthy <mick@mccarthy.tech>
2020-01-31 16:28:39 +0000

    Set up POST book functionality

---
+++ books/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('./book')
+const Book = mongoose.model("Book");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://admin:admin@bookservice-k3mbd.mongodb.net/test?retryWrites=true&w=majority', { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    console.log('Book service db connected successfully')
+);
+
+app.listen(4545, () => {
+    console.log('Book service listening on port 4545');
+});
+
+app.get('/', (req, res) => {
+    res.send('Root endpoint');
+});
+
+app.post('/book', (req, res) => {
+    let newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        numPages: req.body.numPages,
+        publisher: req.body.publisher
+    };
+
+    let book = new Book(newBook);
+
+    book.save().then(() => {
+        console.log('New book created')
+    }).catch((err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    res.send("Book created successfully");
+});
